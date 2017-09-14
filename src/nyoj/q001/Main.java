4c355437876bc35da6c7dd44c4e38778928ac816
zuolonglong <zuolonglong@e-future.com.cn>
2017-09-14 18:04:15 +0800

    初始导入

---
+++ src/nyoj/q001/Main.java
@@ -0,0 +1,19 @@
+package nyoj.q001;
+
+/**
+ * A+B Problem 
+ * http://acm.nyist.net/JudgeOnline/problem.php?pid=1
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String args[]) throws Exception {
+		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+		String line = stdin.readLine();
+		StringTokenizer st = new StringTokenizer(line);
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(a + b);
+	}
+}
---
+++ src/nyoj/q002/Main.java
@@ -0,0 +1,17 @@
+package nyoj.q002;
+
+/**
+ * 括号配对问题 
+ * http://acm.nyist.net/JudgeOnline/problem.php?pid=2
+ */
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.next();
+		System.out.println(line);
+		
+		scanner.close();
+	}
+}
